05962d4b94ad7b6b07eeee898a944e12f1b0b1b4
will.abson@alfresco.com <will.abson@alfresco.com@62dc7571-1a4c-61d8-ea18-1fca470cc1ef>
2012-11-10 00:33:12 +0000

    Add a CMIS widget to the plugin, allowing admins to add any desired folders to the sidebar. As a result it is no longer necessary to edit the template files after installing the plugin.
    git-svn-id: http://wordpress-cmis-plugin.googlecode.com/svn/trunk@4 62dc7571-1a4c-61d8-ea18-1fca470cc1ef

--- cmis.php
+++ cmis.php
@@ -35,6 +35,9 @@ include('cmis_repository_wrapper.php');
 add_action('admin_menu', 'cmis_admin_actions');
 add_action('plugins_loaded', 'cmis_display_actions');
 add_action('get_header', 'cmis_set_head');
+// register Foo_Widget widget
+add_action('widgets_init', create_function('', 'register_widget("cmis_folder_widget");'));
+add_filter('the_content', 'cmis_parse', 4);
 
 function cmis_admin() {
 	include('cmis_admin.php');
@@ -91,10 +94,12 @@ function cmis_listdocuments() {
 	return $ret;
 }
 
-function build_list($objs, $folder) {
+function build_list($objs, $folder, $folders=TRUE) {
 
 	$objlist = '<ul>';
-	$objlist .= '<li class=folder><a href=' . get_option('siteurl') . '?cmis_show_folder=' . substr($folder->properties['cmis:path'], 0, -strlen(strrchr($folder->properties['cmis:path'], "/"))) . '>Parent folder</a></li>';
+        if ($folders===TRUE) {
+		$objlist .= '<li class=folder><a href=' . get_option('siteurl') . '?cmis_show_folder=' . substr($folder->properties['cmis:path'], 0, -strlen(strrchr($folder->properties['cmis:path'], "/"))) . '>Parent folder</a></li>';
+	}
     foreach ($objs->objectList as $obj)
     {
         if ($obj->properties['cmis:baseTypeId'] == 'cmis:document')
@@ -102,7 +107,7 @@ function build_list($objs, $folder) {
         	$liclass = get_li_class($obj->properties['cmis:contentStreamMimeType']);
             $objlist .= '<li class='.$liclass.'>' . build_doc_link($obj->properties['cmis:objectId'], $obj->properties['cmis:name']) . '</li>';
         }
-        elseif ($obj->properties['cmis:baseTypeId'] == 'cmis:folder')
+        elseif ($obj->properties['cmis:baseTypeId'] == 'cmis:folder' && $folders === TRUE)
         {
             $objlist .= '<li class=folder>' . build_folder_link($obj->properties['cmis:path'], $obj->properties['cmis:name']) . '</li>';
         }
@@ -154,4 +159,131 @@ function build_folder_link($path, $obj_name) {
 
 	return $linky;
 }
+
+function cmis_parse($the_content, $doExcerpt=false) {
+	$pattern = '|\[cmis:(\/[\w\/ ]+)\]|';
+	preg_match_all($pattern, $the_content, $matches,  PREG_SET_ORDER);
+	foreach($matches as $match) {
+		$client = new CMISService(get_option('cmis_repo_url'), get_option('cmis_user'), get_option('cmis_pass'));
+		$folder = $client->getObjectByPath(urlencode($match[1]));
+		$objs = $client->getChildren($folder->id);
+		$new_content = build_list($objs, $folder, FALSE);
+		$the_content = str_replace($match[0], $new_content, $the_content);
+	}
+	return $the_content;
+}
+
+/**
+ * Adds Foo_Widget widget.
+ */
+class CMIS_Folder_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		'cmis_folder_widget', // Base ID
+			'CMIS Folder', // Name
+			array( 'description' => __( 'Display the contents of a CMIS folder', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title;
+		//echo __( 'Hello, World!', 'text_domain' );
+		$showfolder = urlencode($instance['folderpath']);
+
+		if (!empty($showfolder)) {
+			$ret = "";
+			try {
+				$client = new CMISService(get_option('cmis_repo_url'), get_option('cmis_user'), get_option('cmis_pass'));
+				$folder = $client->getObjectByPath($showfolder);
+				if ($folder) {
+					if (empty($title)) {
+						$title = $folder->properties["cmis:name"];
+						echo $before_title . $title . $after_title;
+					}
+					$objs = $client->getChildren($folder->id);
+					$ret .= build_list($objs, $folder, FALSE);
+				}
+				else {
+					$ret = "Folder not found: " . $showfolder;
+				}
+			} catch (Exception $e) {
+				$ret = "Error retrieving documents: $e";
+			}
+			echo $ret;
+		}
+
+		echo $after_widget;
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$folderpath = $new_instance['folderpath'];
+		if (strpos($folderpath, "/") !== 0) {
+			$folderpath = "/".$folderpath;
+		}
+		$instance['folderpath'] = $folderpath;
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if (!empty( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			//$title = __('New title', 'text_domain');
+			$title = __('', 'text_domain');
+		}
+		if ( isset( $instance[ 'folderpath' ] ) ) {
+			$folderpath = $instance[ 'folderpath' ];
+		}
+		else {
+			$folderpath = __(get_option('cmis_display_folder'), 'text_domain');
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title (Optional):' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<label for="<?php echo $this->get_field_id('folder'); ?>"><?php _e( 'Folder:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('folderpath'); ?>" name="<?php echo $this->get_field_name('folderpath'); ?>" type="text" value="<?php echo esc_attr($folderpath); ?>" />
+		</p>
+		<?php 
+	}
+
+} // class CMIS_Folder_Widget
 ?>
